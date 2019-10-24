1e54df635f47cdc48aa82f26c90ddeacc3a74f5b
Agarwal, Ankit-CW <Ankit.Agarwal@bestbuy.com>
2019-10-24 15:34:28 -0700

    sorts

---
+++ src/BubbleSort.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+public class BubbleSort {
+
+    //int[] arr = {1, 6, 2, 12, 4, 7};
+
+    public static int[] bub(int[] input){
+        int len =input.length;
+        int temp=0;
+        for(int i=0; i<len; i++){
+            for(int j=0; j<len-i-1;j++){
+                if(input[j]<input[j+1]){
+                }else{
+                    temp=input[j+1];
+                    input[j+1]=input[j];
+                    input[j]=temp;
+                }
+            }
+        }
+        return input;
+
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+
+    public static void main (String[] args){
+        int[] arr = {1, 6, 2, 12, 4, 7,10};
+    }
+
+
+}
