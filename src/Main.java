4154b4ebf295f35a7a81eab6bf64a2321a10ea25
Agarwal, Ankit-CW <Ankit.Agarwal@bestbuy.com>
2019-10-26 19:34:36 -0700

    comparing 2 strings

--- src/Main.java
+++ src/Main.java
@@ -4,17 +4,8 @@ public class Main {
 
 
     public static void main (String[] args) {
-        int[] arr={2,4,1,10,12,24,2,18};
-        Arrays.sort(arr);
-        BinarySearch.binSearchRecursive(arr,0,arr.length-1, 24);
 
 
     }
-
-
-
-
-
-
 
 }
